984e76708bcaac54ae8d7458fe49eda9a667970b
chltjdrhd777 <58500558+chltjdrhd777@users.noreply.github.com>
2020-04-27 16:36:29 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
-createGlobalStyle`
-  ${reset};
-  body{
-    background-color: #ecf0f1;
-  } 
-`;
 
 ReactDOM.render(
   <React.StrictMode>
